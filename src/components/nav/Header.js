0a65fcfdcdf8544c6ce96c87d875a52142948df3
johnvoyage <jmyork762@gmail.com>
2018-10-10 00:04:41 -0400

    change from /dashboard path no longer used

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -9,7 +9,7 @@ const Header = ({ startLogout }) => (
   <header className='header'>
     <div className='content-container'> 
       <div className='header__content'>
-        <Link className='header__title' to='/dashboard'>
+        <Link className='header__title' to='/transactions'>
           <h1>Mazuma</h1>
         </Link>
         <button onClick={startLogout}>Logout</button>

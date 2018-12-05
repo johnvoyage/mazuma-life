c2c4e9ee8f7d54018786c9389ab29788a8df2138
johnvoyage <jmyork762@gmail.com>
2018-12-04 21:58:51 -0500

    entries form aesthetics

--- src/components/entries/EntriesForm.js
+++ src/components/entries/EntriesForm.js
@@ -40,56 +40,59 @@ class EntriesForm extends React.Component {
       <div style={{padding: '5%'}}>
         <label>Name</label>
         <input
-        name='name'
-        onChange={this.onInputChange}
-        placeholder='name'
-        value={fieldInputs.name}
+          name='name'
+          onChange={this.onInputChange}
+          placeholder='name'
+          value={fieldInputs.name}
         />
         <br />
         <label>Description</label>
         <textarea
         name='description'
-        onChange={this.onInputChange} type='text'
-        placeholder='description'
-        value={fieldInputs.term}
+          onChange={this.onInputChange} type='text'
+          placeholder='description'
+          value={fieldInputs.term}
         />
         <br />
         <div style={{background: 'black', padding: '2%'}}>
           <div style={{background: 'white', padding: '2%', display: 'inline-block'}}>
             <label>Accounts</label>
             <input
-            name='accounts'
-            onChange={this.onInputChange} type='text'
-            placeholder='accounts'
-            value={fieldInputs.accounts}
+              name='accounts'
+              onChange={this.onInputChange} type='text'
+              placeholder='accounts'
+              value={fieldInputs.accounts}
             />
             <label>Debit(s)</label>
             <input
-            name='debits'
-            onChange={this.onInputChange} type='text'
-            placeholder='debits'
-            value={fieldInputs.debits}
+              name='debits'
+              onChange={this.onInputChange} type='text'
+              placeholder='debits'
+              value={fieldInputs.debits}
             />
+            <button name='debit-button'>Add</button>
           </div>
           <div style={{background: 'grey', padding: '2%', display: 'inline-block'}}>
             <label>Accounts</label>
             <input
-            name='accounts'
-            onChange={this.onInputChange} type='text'
-            placeholder='accounts'
-            value={fieldInputs.accounts}
+              name='accounts'
+              onChange={this.onInputChange} type='text'
+              placeholder='accounts'
+              value={fieldInputs.accounts}
             />
             <label>Credit(s)</label>
             <input
-            name='credits'
-            onChange={this.onInputChange} type='text'
-            placeholder='credits'
-            value={fieldInputs.credits}
+              name='credits'
+              onChange={this.onInputChange} type='text'
+              placeholder='credits'
+              value={fieldInputs.credits}
             />
+            <button name='credit-button'>Add</button>
           </div>
+          <br />
           <button
-          onClick={this.handleNewAccount}
-          disabled={true}
+            onClick={this.handleNewAccount}
+            disabled={true}
           >
           Add
           </button>

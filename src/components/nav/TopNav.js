7cb0d3eb2a1b0a98b43e50f7653403d11504abe5
johnvoyage <jmyork762@gmail.com>
2019-03-15 23:24:25 -0400

    centered links

--- src/components/nav/TopNav.js
+++ src/components/nav/TopNav.js
@@ -5,13 +5,25 @@ import { connect } from 'react-redux'
 
 
 const TopNav = () => (
-  <div className='nav-bar-container' style={{alignContent: 'justify'}}>
-    <Link to='/profile'>Profile</Link>
-    <Link to='/entries'>Entries</Link>
-    <Link to='/accounts'>Accounts</Link>
-    <Link to='/goals'>Goals</Link>
-    <Link to='/charts'>Charts</Link>
-    <Link to='/table/amortization'>Amortization Table</Link>
+  <div className='nav-bar-container'>
+    <div className='nav-bar-link'>
+      <Link to='/profile'>Profile</Link>
+    </div>
+    <div className='nav-bar-link'>
+      <Link to='/entries'>Entries</Link>
+    </div>
+    <div className='nav-bar-link'>
+      <Link to='/accounts'>Accounts</Link>
+    </div>
+    <div className='nav-bar-link'>
+      <Link to='/goals'>Goals</Link>
+    </div>
+    <div className='nav-bar-link'>
+      <Link to='/charts'>Charts</Link>
+    </div>
+    <div className='nav-bar-link'>
+      <Link to='/table/amortization'>Amortization Table</Link>
+    </div>
   </div>
 )
 
